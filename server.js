bcf25dfcf33b28b586c09c24024d6e38e7d34217
Theresa <TreeGee73@gmail.com>
2021-01-22 22:01:51 -0700

    reviewed and commented

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+// Dependancies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = 3000;
-
 const app = express();
 
+// Middleware
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Make express serve static files
 app.use(express.static("public"));
 
+// DB connection
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
@@ -23,6 +25,8 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
+// Start the server and begin listening for client requests.
 app.listen(PORT, () => {
+  // Server log to indicate the server has started
   console.log(`App running on port ${PORT}!`);
 });
